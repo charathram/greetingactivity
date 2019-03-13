a99a6692fb54922e0ca1bee34434ea8f4df0b71e
Charath Ranganathan <charath@pfactor.io>
2019-03-13 07:31:51 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# 	greetingactivity - Activity
---
+++ activity.go
@@ -0,0 +1,37 @@
+package greetingactivity
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("activity-greeting")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+	// get activity data from context
+	name := context.GetInput("name").(string)
+
+	// do eval
+	log.Infof("Activity greetingactivity: [%s]", name)
+
+	// Set the return value
+	context.SetOutput("greeting", "Hello "+name)
+
+	return true, nil
+}
